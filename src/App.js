85d503b44c3eb9991ef8f07be4c647289c0627de
minas <minasmalxas@gmail.com>
2022-03-02 22:23:39 +0400

    Task Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { Box, Button, HStack, VStack, Stack, Grid, Flex } from '@chakra-ui/react'
+import uuid from 'react-uuid'
+import {CloseIcon} from '@chakra-ui/icons'
 
-function App() {
+const App = () => {
+
+  const [cards, setCards] = useState([])
+
+  const create = () => {
+    const newCard = {id: uuid(), num: Math.floor(Math.random()*1000)}
+    setCards([...cards, newCard]) 
+  }
+
+  const onDelete = (id) => {
+    const filteredCards = cards.filter((item) => item.id !== id)
+    setCards(filteredCards)
+  }
+
+  const onSort = () => {
+    const sorted = cards.sort((a, b) => a.num - b.num)
+    setCards([...sorted])
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Stack direction="row" spacing="5">
+    <Box ml="30px" minW="65rem">
+      <HStack spacing="5" 
+      borderBottom='1px solid black' 
+      pb="10px" pt="10px">
+   <Button onClick={() => create()}>
+   Add Card
+   </Button>
+   <Button onClick={() => onSort()}>
+   Sort Card
+   </Button>
+   </HStack>
+   <Box mx="50px" h="80vh"
+   overflowY="scroll">
+   <Grid templateColumns='repeat(2, 1fr)'
+   gap={5} pt="20px">
+   {cards.map((item) => 
+   <Box border="1px solid grey" w="300px">
+     <Box float="right" m="5px"
+     onClick={() => onDelete(item.id)}>
+     <CloseIcon/>
+     </Box>
+   <Flex h="200px"
+    alignItems="center" 
+    justifyContent="center">
+     <Box fontWeight="bold">{item.num}</Box>
+     </Flex>
+     </Box>)}
+      </Grid>
+      </Box>
+   </Box>
+   <Flex 
+   borderLeft="1px solid grey" h="100vh"
+   justifyContent="center"
+    alignItems="center">
+   <Box m="4px" fontWeight="bold">
+     Press the "Add Card" button to add the new Card.
+     Use the "Sort Cards" button to sort the Cards by the Increase.
+     Press an X icon on the top right to delete them.
+   </Box>
+   </Flex>
+   </Stack>
+  )
 }
 
-export default App;
+export default App
